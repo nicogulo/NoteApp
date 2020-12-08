e50ee62f68c381a9ca517fde426275b0c74d31b9
nicogulo <nicogulobelas@gmail.com>
2020-12-08 20:14:47 +0700

    push

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655334284-yPKqQ3ql";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655334284-mgoKVaKj";   // change the default LIFF value if you are not using a node server
  
     // DO NOT CHANGE THIS
     let myLiffId = "";
